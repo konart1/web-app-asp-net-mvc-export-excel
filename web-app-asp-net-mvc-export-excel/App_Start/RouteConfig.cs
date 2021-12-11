cf27ba5a9f932a194942cc45e8c3d4c8b3199c87
konart1 <91774199+konart1@users.noreply.github.com>
2021-12-11 15:24:36 +1000

    fix

--- web-app-asp-net-mvc-export-excel/App_Start/RouteConfig.cs
+++ web-app-asp-net-mvc-export-excel/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace web_app_asp_net_mvc_export_excel
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Product", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
